3840e4b6c647aff640ba713aa1a301fba9311d98
christophershin <dinojun323@gmail.com>
2025-11-10 10:31:44 -0500

    weapons

--- Practical Exam/Assets/scripts/playerinteractions.cs
+++ Practical Exam/Assets/scripts/playerinteractions.cs
@@ -24,7 +24,7 @@ public class playerinteractions : MonoBehaviour
             animator.SetBool("Walking", true);
         }
 
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKey(KeyCode.E))
         {
             Application.Quit();
         }
